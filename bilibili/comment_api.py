ccae9ee8cda0792bdc57cfcc6bdcf54f6f87058b
gj94gj94ao4 <gj94gj94ao4@gmail.com>
2018-07-24 03:52:32 +0800

    新增comment_api測試與功能

--- bilibili/comment_api.py
+++ bilibili/comment_api.py
@@ -10,7 +10,7 @@ def checked(fn):
     def rf(*args, **kwargs):
         if len(CHAT_RES_LIST) == 0:
             _get_all_av()
-        fn(*args, **kwargs)
+        return fn(*args, **kwargs)
     return rf
 
 
@@ -19,7 +19,6 @@ def _get_all_av():
     CHAT_RES_LIST = os.listdir('.')
     if len(CHAT_RES_LIST) == 0:
         raise Exception("資料陣列無內容")
-    os.chdir("..")
 
 # TODO:返還{"time":"comment", ... }
 @checked
@@ -28,7 +27,9 @@ def get_av_comments_list(av, cid=""):
     用av號找尋該影片所有的留言與時軸
     若有多p則必須給cid，不然就會返還
     第一個
+    (sec,user,text)
     """
+    print(os.getcwd())
     os.chdir(av)
     try:
         if len(cid) == 0:
@@ -39,7 +40,17 @@ def get_av_comments_list(av, cid=""):
         print("該AV資料夾中無檔案")
     tree = ET.parse(cid)
     root = tree.getroot()
-    return root.items()
+    users = []
+    texts = []
+    secs = []
+    for c in root:
+        if c.tag == "d":
+            secs.append(c.attrib["p"].split(",")[0])
+            users.append(c.attrib["p"].split(",")[6])
+            texts.append(c.text)
+    return (secs,users,texts)
 
-    
-
+if __name__ == "__main__":
+    c = get_av_comments_list("av27436999")
+    for sec, user, text in zip(c[0], c[1], c[2]):
+        print(sec,user,text)
--- bilibili_test/comment_api_test.py
+++ bilibili_test/comment_api_test.py
@@ -1,9 +1,16 @@
 import sys
 sys.path.append("../")
+import os
 
 import pytest
 from bilibili.comment_api import *
 
 NULL_PATH = "02D20BBD7E394A/"
+AV_NUMBER = "av27436999"
+TEST_XML_RES = "chat_xml_res"
 
-
+def test_get_av_comments_list():
+    os.chdir(__file__ + "/../")
+    CHAT_XML_DIR = TEST_XML_RES
+    x = get_av_comments_list(AV_NUMBER)
+    assert x[1][1] == "9ee3cac2"
